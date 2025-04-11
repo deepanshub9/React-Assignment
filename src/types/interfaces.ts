d6d7d2cab904faea61be39643df317d44a6ff390
Deepanshu <kukukumar1996@gmail.com>
2025-04-11 14:01:42 +0100

    Movie Details Page rendering.

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -23,3 +23,17 @@ export interface BaseMovieListProps {
 export interface MovieDetailsProps extends BaseMovieProps {
   genres: { id: number; name: string }[];
 }
+export interface MovieImage {
+  file_path: string;
+  aspect_ratio?: number;
+  height?: number;
+  iso_639_1?: string;
+  vote_average?: number;
+  vote_count?: number;
+  width?: number;
+}
+
+export interface MoviePageProps {
+  movie: MovieDetailsProps;
+  images: MovieImage[];
+}
