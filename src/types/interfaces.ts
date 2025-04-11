37104bcf646ff1ec16fccb615768da3e647cc970
Deepanshu <kukukumar1996@gmail.com>
2025-04-11 13:51:38 +0100

    Implemented MovieDetails and MovieHeader components.

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -20,3 +20,6 @@ export interface BaseMovieProps {
 export interface BaseMovieListProps {
   movies: BaseMovieProps[];
 }
+export interface MovieDetailsProps extends BaseMovieProps {
+  genres: { id: number; name: string }[];
+}
